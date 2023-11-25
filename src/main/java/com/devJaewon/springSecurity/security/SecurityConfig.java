5bc377c5b787af8d79e9b8ad28f51bea048f1d39
dev-Jaewon <app235naver@gmail.com>
2023-11-26 03:40:29 +0900

    modified h2 db 사용위해설정

--- src/main/java/com/devJaewon/springSecurity/security/SecurityConfig.java
+++ src/main/java/com/devJaewon/springSecurity/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -54,6 +55,6 @@ public class SecurityConfig {
 
     @Bean
     WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/assets/**", "/static/**");
+        return (web) -> web.ignoring().requestMatchers("/h2-console/**");
     }
 }
