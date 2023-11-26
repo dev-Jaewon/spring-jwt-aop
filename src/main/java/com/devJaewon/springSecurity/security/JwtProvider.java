ea7be35d1867ecabcf0588113887d291a5246be7
dev-Jaewon <app235naver@gmail.com>
2023-11-26 16:20:48 +0900

    feat jwtProvider

---
+++ src/main/java/com/devJaewon/springSecurity/security/JwtProvider.java
@@ -0,0 +1,61 @@
+package com.devJaewon.springSecurity.security;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import javax.crypto.SecretKey;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+public class JwtProvider {
+    private String SECRET_KEY = "Hello World. It's a Beautiful Day!";
+    private String token = "";
+
+    public JwtProvider() {
+    }
+
+    public JwtProvider(String token) {
+        this.token = token;
+    }
+
+    public String createAccessToken(String user) {
+        return createToken(user, System.currentTimeMillis() + 1000 * 60 * 10);
+    }
+
+    public String createRefreshToken(String user) {
+        return createToken(user, System.currentTimeMillis() + 1000 * 60 * 120);
+    }
+
+    public SecretKey getSecretKey() {
+        return Keys.hmacShaKeyFor(this.SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String createToken(String userId, long exprieTime) {
+        return Jwts.builder()
+                // .claims(claims)
+                .subject(userId)
+                .issuer("dev-jaewon")
+                .expiration(new Date(exprieTime))
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .signWith(getSecretKey(), Jwts.SIG.HS256)
+                .compact();
+    }
+
+    public String getUserName() {
+        return Jwts
+                .parser()
+                .verifyWith(getSecretKey())
+                .build()
+                .parseSignedClaims(this.token)
+                .getPayload()
+                .getSubject();
+    }
+
+    // public Authentication getAuthentication() {
+    // User user = new User(getUserName(), "", Arrays.asList(new
+    // SimpleGrantedAuthority("user")));
+
+    // return UsernamePasswordAuthenticationToken.authenticated(user, this.token,
+    // user.getAuthorities());
+    // }
+}
